f0b2e90beb665887aadef3ebb741533cb74eb7f0
Gastr <32821943+Gastr@users.noreply.github.com>
2017-12-29 17:33:26 -0800

    positioning

--- bar.js
+++ bar.js
@@ -11,9 +11,9 @@ function Bar() {
     //rect(bars.indexOf(this) * this.width - (width / 2), height - this.val - (height / 2), this.width, this.val);
 
     push();
-
+    rotateX(-0.05);
     fill(this.fill[0], this.fill[1], this.fill[2]);
-    translate(bars.indexOf(this) * this.width - (width / 2) + 4, -(this.val / 2) + width / 2, -50);
+    translate(bars.indexOf(this) * this.width - (width / 2), -(this.val / 2) + height / 2 + 30, -50);
     box(this.width, this.val);
     pop();
   }
